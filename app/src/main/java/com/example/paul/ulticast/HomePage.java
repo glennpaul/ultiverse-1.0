48959d3960df608e070da610e9491e510499f020
Paul <glenn.paul@live.ca>
2017-10-22 11:10:26 -0400

    Added functionality to bring user straight to disc central if already loggen in previously.

--- app/src/main/java/com/example/paul/ulticast/HomePage.java
+++ app/src/main/java/com/example/paul/ulticast/HomePage.java
@@ -3,14 +3,44 @@ package com.example.paul.ulticast;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+
 public class HomePage extends AppCompatActivity {
+
+
+    //get authorization reference
+    private FirebaseAuth mAuth;
+    String user_UID;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_page);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        if (mAuth.getCurrentUser() != null) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        } else {
+            setContentView(R.layout.home_page);
+        }
+
+
     }
+
+
+
+
+
+
     /** Called when the user taps the LOGIN button */
     public void log_in(View view) {
         Intent intent = new Intent(this, LoginActivity.class);
